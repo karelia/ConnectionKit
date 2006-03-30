31111737e4474913d5c699e797ab1ecdaadb0b26
Greg Hulands <ghulands@me.com>
2006-03-30 23:32:23 +0000

    Complete implementation of the MultipleConnection class

--- MultipleConnection.h
+++ MultipleConnection.h
@@ -1,10 +1,31 @@
-//
-//  MultipleConnection.h
-//  FTPConnection
-//
-//  Created by Greg Hulands on 9/01/06.
-//  Copyright 2006 __MyCompanyName__. All rights reserved.
-//
+/*
+ Copyright (c) 2004, Greg Hulands <ghulands@framedphotographics.com>
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification, 
+ are permitted provided that the following conditions are met:
+ 
+ Redistributions of source code must retain the above copyright notice, this list 
+ of conditions and the following disclaimer.
+ 
+ Redistributions in binary form must reproduce the above copyright notice, this 
+ list of conditions and the following disclaimer in the documentation and/or other 
+ materials provided with the distribution.
+ 
+ Neither the name of Greg Hulands nor the names of its contributors may be used to 
+ endorse or promote products derived from this software without specific prior 
+ written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+ OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+ SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
+ TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+ BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY 
+ WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import <Cocoa/Cocoa.h>
 #import "AbstractQueueConnection.h"
@@ -15,11 +36,16 @@
 	all the connections and provide delegate notifications as if it were a single
 	connection. This class does not register itself as it is a wrapper class and
 	does not service any one particular protocol and therefore is used programmatically.
+ 
+	NB: It only supports uploading and not downloading
 */
 
 @interface MultipleConnection : AbstractQueueConnection 
 {
 	NSMutableArray *_connections;
+	
+	NSMutableArray *_connectedConnections;
+	NSMutableArray *_folderCreations;
 }
 
 // Designated initializer
