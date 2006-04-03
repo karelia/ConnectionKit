d9abab34bb382936e262d2797c7bac3b41158ffc
Greg Hulands <ghulands@me.com>
2006-04-03 09:01:53 +0000

    Created new NSData category removing old one
    git-svn-id: http://opensource.utr-software.com/source/connection/trunk@23 30aebe3c-6c0d-0410-bd16-8e6c7f3d8b55

---
+++ NSData+Connection.h
@@ -0,0 +1,16 @@
+//
+//  NSData+Connection.h
+//  FTPConnection
+//
+//  Created by Greg Hulands on 3/04/06.
+//  Copyright 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface NSData (Connection)
+
+- (NSString *)base64Encoding;
+
+@end
