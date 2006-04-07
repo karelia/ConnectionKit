e01df592fbbe4d3bcdda707bfbb048b896016334
Greg Hulands <ghulands@me.com>
2006-04-07 00:14:41 +0000

    I have made the file connection multi threaded so it behaves exactly the same as the other connection types.
    git-svn-id: http://opensource.utr-software.com/source/connection/trunk@37 30aebe3c-6c0d-0410-bd16-8e6c7f3d8b55

--- FileConnection.m
+++ FileConnection.m
@@ -33,9 +33,12 @@
  */
 
 #import "FileConnection.h"
+#import "RunLoopForwarder.h"
+#import "InterThreadMessaging.h"
 
 NSString *FileConnectionErrorDomain = @"FileConnectionErrorDomain";
 
+enum { CONNECT = 0, COMMAND, ABORT, CANCEL_ALL, DISCONNECT, FORCE_DISCONNECT, KILL_THREAD };		// port messages
 
 @implementation FileConnection
 
@@ -103,9 +106,167 @@ NSString *FileConnectionErrorDomain = @"FileConnectionErrorDomain";
 {
 	if (self = [super initWithHost:host port:port username:username password:password])
 	{
-		[self changeToDirectory:NSHomeDirectory()];
+		myPendingInvocations = [[NSMutableArray array] retain];
+		myCurrentDirectory = [[NSString alloc] initWithString:NSHomeDirectory()];
+		
+		myLock = [[NSLock alloc] init];
+		myForwarder = [[RunLoopForwarder alloc] init];
+		myPort = [[NSPort port] retain];
+		[myPort setDelegate:self];
+		
+		[NSThread prepareForInterThreadMessages];
+		[NSThread detachNewThreadSelector:@selector(runFileConnectionBackgroundThread:) toTarget:self withObject:nil];
 	}
 	return self;
+}
+
+- (void)dealloc
+{
+	[self sendPortMessage:KILL_THREAD];
+	[myPort setDelegate:nil];
+	[myPort release];
+	[myLock release];
+	[myForwarder release];
+	
+	[myPendingInvocations release];
+	[myCurrentDirectory release];
+	[super dealloc];
+}
+#pragma mark -
+#pragma mark Threading Support
+
+- (void)runFileConnectionBackgroundThread:(id)notUsed
+{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	_bgThread = [NSThread currentThread];
+	[NSThread prepareForInterThreadMessages];
+	// NOTE: this may be leaking ... there are two retains going on here.  Apple bug report #2885852, still open after TWO YEARS!
+	// But then again, we can't remove the thread, so it really doesn't mean much.
+	[[NSRunLoop currentRunLoop] addPort:myPort forMode:(NSString *)kCFRunLoopCommonModes];
+	[[NSRunLoop currentRunLoop] run];
+	
+	[pool release];
+}
+
+- (void)sendPortMessage:(int)aMessage
+{
+	if (nil != myPort)
+	{
+		NSPortMessage *message
+		= [[NSPortMessage alloc] initWithSendPort:myPort
+									  receivePort:myPort components:nil];
+		[message setMsgid:aMessage];
+		
+		@try {
+			BOOL sent = [message sendBeforeDate:[NSDate dateWithTimeIntervalSinceNow:15.0]];
+			if (!sent)
+			{
+				if ([AbstractConnection debugEnabled])
+					NSLog(@"FileConnection couldn't send message %d", aMessage);
+			}
+		} @catch (NSException *ex) {
+			NSLog(@"%@", ex);
+		} @finally {
+			[message release];
+		} 
+	}
+}
+
+- (void)handlePortMessage:(NSPortMessage *)portMessage
+{
+	int message = [portMessage msgid];
+	
+	switch (message)
+	{
+		case CONNECT:
+		{
+			if ( _flags.didConnect )
+			{
+				[myForwarder connection:self didConnectToHost:_connectionHost];
+			}
+			_flags.isConnected = YES;
+			break;
+		}
+		case COMMAND:
+		{
+			[self processInvocations];
+			break;
+		}
+		case ABORT:
+			if ( _flags.cancel )
+			{
+				[myForwarder connectionDidCancelTransfer:self];
+			}
+			[self processInvocations];
+			break;
+			
+		case CANCEL_ALL:
+			[myLock lock];
+			[myPendingInvocations removeAllObjects];
+			[myLock unlock];
+			if ( _flags.cancel )
+			{
+				[myForwarder connectionDidCancelTransfer:self];
+			}
+			break;
+		case FORCE_DISCONNECT:
+		{
+			[myLock lock];
+			[myPendingInvocations removeAllObjects];
+			[myLock unlock];
+			if ( _flags.cancel )
+			{
+				[myForwarder connectionDidCancelTransfer:self];
+			}
+			_flags.isConnected = NO;
+			if (_flags.didDisconnect)
+			{
+				[myForwarder connection:self didDisconnectFromHost:[self host]];
+			}
+		}
+		break;
+		case KILL_THREAD:
+		{
+			[[NSRunLoop currentRunLoop] removePort:myPort forMode:(NSString *)kCFRunLoopCommonModes];
+			break;
+		}
+	}
+}
+
+- (void)setDelegate:(id)delegate
+{
+	[super setDelegate:delegate];
+	[myForwarder setDelegate:delegate];
+}
+
+#pragma mark -
+#pragma mark Invocation Queue
+
+- (void)processInvocations
+{
+	NSAssert([NSThread currentThread] == _bgThread, @"Processing Invocations from wrong thread");
+	
+	[myLock lock];
+    while ( (nil != myPendingInvocations) && ([myPendingInvocations count] > 0) )
+	{
+		NSInvocation *invocation = [myPendingInvocations objectAtIndex:0];
+		if ( nil != invocation )
+		{
+			[invocation retain];
+			[myPendingInvocations removeObjectAtIndex:0];
+			[invocation invoke];
+			[invocation release];
+		}
+	}
+	[myLock unlock];
+}
+
+- (void)queueInvocation:(NSInvocation *)inv
+{
+	[myLock lock];
+	[myPendingInvocations addObject:inv];
+	[myLock unlock];
+	[self sendPortMessage:COMMAND];
 }
 
 #pragma mark -
@@ -115,44 +276,60 @@ NSString *FileConnectionErrorDomain = @"FileConnectionErrorDomain";
 */
 - (void)connect
 {
-	_flags.isConnected = YES;
-	if (_flags.didConnect)
-		[_delegate connection:self didConnectToHost:_connectionHost];
+	[self sendPortMessage:CONNECT];
 }
 
 /*!	Basically a no-op, just send the completion method.
 */
 
+- (void)fcDisconnect
+{
+	if (_flags.didDisconnect)
+	{
+		[myForwarder connection:self didDisconnectFromHost:[self host]];
+	}
+	_flags.isConnected = NO;
+}
+
 - (void)disconnect
 {
-	_flags.isConnected = NO;
-	if (_flags.didDisconnect)
-		[_delegate connection:self didDisconnectFromHost:_connectionHost];
-
+	NSInvocation *inv = [NSInvocation invocationWithSelector:@selector(fcDisconnect)
+													  target:self
+												   arguments:[NSArray array]];
+	[self queueInvocation:inv];
 }
 
 - (void)forceDisconnect
 {
-	[self disconnect];
+	[self sendPortMessage:FORCE_DISCONNECT];
 }
 
-- (void)changeToDirectory:(NSString *)aDirectory	// an absolute directory
+- (void)fcChangeToDirectory:(NSString *)aDirectory
 {
 	[self setCurrentOperation:kChangeToDirectory];
-
+	
 	NSFileManager *fm = [NSFileManager defaultManager];
-
+	
 	BOOL success = [fm changeCurrentDirectoryPath:aDirectory];
 	if (success && _flags.changeDirectory)
 	{
 		[_delegate connection:self didChangeToDirectory:aDirectory];
 	}
+	[myCurrentDirectory autorelease];
+	myCurrentDirectory = [aDirectory copy];
+}
+
+- (void)changeToDirectory:(NSString *)aDirectory	// an absolute directory
+{
+	NSInvocation *inv = [NSInvocation invocationWithSelector:@selector(fcChangeToDirectory:)
+													  target:self
+												   arguments:[NSArray arrayWithObjects:aDirectory, nil]];
+	[self queueInvocation:inv];
 }
 
 - (NSString *)currentDirectory
 {
-	NSFileManager *fm = [NSFileManager defaultManager];
-	return [fm currentDirectoryPath];
+	return myCurrentDirectory;
 }
 
 - (void)createDirectory:(NSString *)aName
@@ -160,10 +337,11 @@ NSString *FileConnectionErrorDomain = @"FileConnectionErrorDomain";
 	[self createDirectory:aName permissions:0];
 }
 
-- (void)createDirectory:(NSString *)aName permissions:(unsigned long)aPermissions
+- (void)fcCreateDirectory:(NSString *)aName permissions:(NSNumber *)perms
 {
 	[self setCurrentOperation:kCreateDirectory];
-
+	unsigned long aPermissions = [perms unsignedLongValue];
+	
 	NSDictionary *fmDictionary = nil;
 	if (0 != aPermissions)
 	{
@@ -171,7 +349,7 @@ NSString *FileConnectionErrorDomain = @"FileConnectionErrorDomain";
 	}
 	NSFileManager *fm = [NSFileManager defaultManager];
 	BOOL success = [fm createDirectoryAtPath:aName attributes:fmDictionary];
-
+	
 	if (success)
 	{
 		if (_flags.createDirectory)
@@ -203,19 +381,28 @@ NSString *FileConnectionErrorDomain = @"FileConnectionErrorDomain";
 	}
 }
 
-- (void)setPermissions:(unsigned long)permissions forFile:(NSString *)path
+- (void)createDirectory:(NSString *)aName permissions:(unsigned long)aPermissions
+{
+	NSInvocation *inv = [NSInvocation invocationWithSelector:@selector(fcCreateDirectory:permissions:)
+													  target:self
+												   arguments:[NSArray arrayWithObjects:aName, [NSNumber numberWithUnsignedLong:aPermissions], nil]];
+	[self queueInvocation:inv];
+}
+
+- (void)fcSetPermissions:(NSNumber *)perms forFile:(NSString *)path
 {
 	[self setCurrentOperation:kSetPermissions];
-
+	unsigned long permissions = [perms unsignedLongValue];
+	
 	NSFileManager *fm = [NSFileManager defaultManager];
-
+	
 	NSDictionary *attributes = [fm fileAttributesAtPath:path traverseLink:YES];	// TODO: verify link is OK
-
+	
 	NSMutableDictionary *newAttr = [NSMutableDictionary dictionaryWithDictionary:attributes];
-
+	
 	[newAttr setObject:[NSNumber numberWithUnsignedLong:permissions] forKey:NSFilePosixPermissions];
 	BOOL success = [fm changeFileAttributes:newAttr atPath:path];
-
+	
 	if (success)
 	{
 		if (_flags.permissions)
@@ -238,10 +425,17 @@ NSString *FileConnectionErrorDomain = @"FileConnectionErrorDomain";
 													  nil]]];
 		}
 	}
-
 }
 
-- (void)rename:(NSString *)fromPath to:(NSString *)toPath
+- (void)setPermissions:(unsigned long)permissions forFile:(NSString *)path
+{
+	NSInvocation *inv = [NSInvocation invocationWithSelector:@selector(fcSetPermissions:forFile:)
+													  target:self
+												   arguments:[NSArray arrayWithObjects:[NSNumber numberWithUnsignedLong:permissions], path, nil]];
+	[self queueInvocation:inv];
+}
+
+- (void)fcRename:(NSString *)fromPath to:(NSString *)toPath
 {
 	[self setCurrentOperation:kRename];
 	NSFileManager *fm = [NSFileManager defaultManager];
@@ -252,7 +446,15 @@ NSString *FileConnectionErrorDomain = @"FileConnectionErrorDomain";
 	}
 }
 
-- (void)deleteFile:(NSString *)path
+- (void)rename:(NSString *)fromPath to:(NSString *)toPath
+{
+	NSInvocation *inv = [NSInvocation invocationWithSelector:@selector(fcRename:to:)
+													  target:self
+												   arguments:[NSArray arrayWithObjects:fromPath, toPath, nil]];
+	[self queueInvocation:inv];
+}
+
+- (void)fcDeleteFile:(NSString *)path
 {
 	[self setCurrentOperation:kDeleteFile];
 	NSFileManager *fm = [NSFileManager defaultManager];
@@ -263,7 +465,15 @@ NSString *FileConnectionErrorDomain = @"FileConnectionErrorDomain";
 	}
 }
 
-- (void)deleteDirectory:(NSString *)dirPath
+- (void)deleteFile:(NSString *)path
+{
+	NSInvocation *inv = [NSInvocation invocationWithSelector:@selector(fcDeleteFile:)
+													  target:self 
+												   arguments:[NSArray arrayWithObject:path]];
+	[self queueInvocation:inv];
+}
+
+- (void)fcDeleteDirectory:(NSString *)dirPath
 {
 	[self setCurrentOperation:kDeleteDirectory];
 	NSFileManager *fm = [NSFileManager defaultManager];
@@ -274,17 +484,32 @@ NSString *FileConnectionErrorDomain = @"FileConnectionErrorDomain";
 	}
 }
 
+- (void)deleteDirectory:(NSString *)dirPath
+{
+	NSInvocation *inv = [NSInvocation invocationWithSelector:@selector(fcDeleteDirectory:)
+													  target:self 
+												   arguments:[NSArray arrayWithObject:dirPath]];
+	[self queueInvocation:inv];
+}
+
 /*!	Upload the given file to the working directory.
 */
+
+- (void)fcUploadFile:(NSString *)localPath
+{
+	[self fcUploadFile:localPath toFile:[myCurrentDirectory stringByAppendingPathComponent:[localPath lastPathComponent]]];
+}
+
 - (void)uploadFile:(NSString *)localPath
 {
-	NSFileManager *fm = [NSFileManager defaultManager];
-	[self uploadFile:localPath toFile:[[fm currentDirectoryPath] stringByAppendingPathComponent:[localPath lastPathComponent]]];
+	NSInvocation *inv = [NSInvocation invocationWithSelector:@selector(fcUploadFile:)
+													  target:self
+												   arguments:[NSArray arrayWithObject:localPath]];
 }
 
 /*!	Copy the given file to the given directory
 */
-- (void)uploadFile:(NSString *)localPath toFile:(NSString *)remotePath
+- (void)fcUploadFile:(NSString *)localPath toFile:(NSString *)remotePath
 {
 	[self setCurrentOperation:kUploadFile];
 	NSFileManager *fm = [NSFileManager defaultManager];
@@ -316,13 +541,22 @@ NSString *FileConnectionErrorDomain = @"FileConnectionErrorDomain";
 	}
 }
 
+- (void)uploadFile:(NSString *)localPath toFile:(NSString *)remotePath
+{
+	NSInvocation *inv = [NSInvocation invocationWithSelector:@selector(fcUploadFile:toFile:)
+													  target:self
+												   arguments:[NSArray arrayWithObjects:localPath, remotePath, nil]];
+	[self queueInvocation:inv];
+}
+
 
 - (void)resumeUploadFile:(NSString *)localPath fileOffset:(long long)offset
 {
 	// Noop, there's no such thing as a partial transfer on a file system since it's instantaneous.
+	[self uploadFile:localPath];
 }
 
-- (void)uploadFromData:(NSData *)data toFile:(NSString *)remotePath
+- (void)fcUploadFromData:(NSData *)data toFile:(NSString *)remotePath
 {
 	[self setCurrentOperation:kUploadFromData];
 	NSFileManager *fm = [NSFileManager defaultManager];
@@ -345,15 +579,25 @@ NSString *FileConnectionErrorDomain = @"FileConnectionErrorDomain";
 	}
 }
 
+- (void)uploadFromData:(NSData *)data toFile:(NSString *)remotePath
+{
+	NSInvocation *inv = [NSInvocation invocationWithSelector:@selector(fcUploadFromData:toFile:)
+													  target:self
+												   arguments:[NSArray arrayWithObjects:data, remotePath, nil]];
+	[self queueInvocation:inv];
+}
+
 - (void)resumeUploadFromData:(NSData *)data toFile:(NSString *)remotePath fileOffset:(long long)offset
 {
 	// Noop, there's no such thing as a partial transfer on a file system since it's instantaneous.
+	[self uploadFromData:data toFile:remotePath];
 }
 
 /*!	Copy the file to the given directory
 */
-- (void)downloadFile:(NSString *)remotePath toDirectory:(NSString *)dirPath overwrite:(BOOL)flag
+- (void)fcDownloadFile:(NSString *)remotePath toDirectory:(NSString *)dirPath overwrite:(NSNumber *)aFlag
 {
+	BOOL flag = [aFlag boolValue];
 	[self setCurrentOperation:kDownloadFile];
 	NSFileManager *fm = [NSFileManager defaultManager];
 	NSString *name = [remotePath lastPathComponent];
@@ -399,33 +643,41 @@ NSString *FileConnectionErrorDomain = @"FileConnectionErrorDomain";
 	}
 }
 
+- (void)downloadFile:(NSString *)remotePath toDirectory:(NSString *)dirPath overwrite:(BOOL)flag
+{
+	NSInvocation *inv = [NSInvocation invocationWithSelector:@selector(fcDownloadFile:toDirectory:overwrite:)
+													  target:self
+												   arguments:[NSArray arrayWithObjects:remotePath, dirPath, [NSNumber numberWithBool:flag], nil]];
+	[self queueInvocation:inv];
+}
+
 - (void)resumeDownloadFile:(NSString *)remotePath toDirectory:(NSString *)dirPath fileOffset:(long long)offset
 {
 	// Noop, there's no such thing as a partial transfer on a file system since it's instantaneous.
+	[self downloadFile:remotePath toDirectory:dirPath overwrite:YES];
 }
 
 - (void)cancelTransfer
 {
-	// Noop, there's no such thing as cancelling on a file system since it's instantaneous.
+	[self sendPortMessage:ABORT];
 }
 
 - (void)cancelAll
 {
-	// Noop, there's no such thing as cancelling on a file system since it's instantaneous, and there's no queue.
+	[self sendPortMessage:CANCEL_ALL];
 }
 
-- (void)directoryContents
+- (void)fcDirectoryContents
 {
 	[self setCurrentOperation:kDirectoryContents];
 	NSFileManager *fm = [NSFileManager defaultManager];
-	NSString *folder = [fm currentDirectoryPath];
-	NSArray *array = [fm directoryContentsAtPath:folder];
+	NSArray *array = [fm directoryContentsAtPath:myCurrentDirectory];
 	NSMutableArray *packaged = [NSMutableArray arrayWithCapacity:[array count]];
 	NSEnumerator *e = [array objectEnumerator];
 	NSString *cur;
 	
 	while (cur = [e nextObject]) {
-		NSString *file = [NSString stringWithFormat:@"%@/%@", folder, cur];
+		NSString *file = [NSString stringWithFormat:@"%@/%@", myCurrentDirectory, cur];
 		NSMutableDictionary *attribs = [NSMutableDictionary dictionaryWithDictionary:[fm fileAttributesAtPath:file
 																								 traverseLink:NO]];
 		[attribs setObject:cur forKey:cxFilenameKey];
@@ -443,7 +695,15 @@ NSString *FileConnectionErrorDomain = @"FileConnectionErrorDomain";
 	}
 }
 
-- (void)contentsOfDirectory:(NSString *)dirPath
+- (void)directoryContents
+{
+	NSInvocation *inv = [NSInvocation invocationWithSelector:@selector(fcDirectoryContents)
+													  target:self
+												   arguments:[NSArray array]];
+	[self queueInvocation:inv];
+}
+
+- (void)fcContentsOfDirectory:(NSString *)dirPath
 {
 	[self setCurrentOperation:kDirectoryContents];
 	NSFileManager *fm = [NSFileManager defaultManager];
@@ -469,6 +729,14 @@ NSString *FileConnectionErrorDomain = @"FileConnectionErrorDomain";
 	{
 		[_delegate connection:self didReceiveContents:packaged ofDirectory:dirPath];
 	}
+}
+
+- (void)contentsOfDirectory:(NSString *)dirPath
+{
+	NSInvocation *inv = [NSInvocation invocationWithSelector:@selector(fcContentsOfDirectory:)
+													  target:self
+												   arguments:[NSArray arrayWithObject:dirPath]];
+	[self queueInvocation:inv];
 }
 
 #pragma mark -
