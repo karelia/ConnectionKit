8bb7e233b7164740969174a9a441c7e2142eb0dd
Olivier Destrebecq <oblivious+odestrebecq@subtlegradient.com>
2006-04-24 16:49:17 +0000

    Added a unit test target added unit test for connection and download of file

---
+++ unit test/LocalConnectionTest.h
@@ -0,0 +1,18 @@
+//
+//  LocalConnectionTest.h
+//  Connection
+//
+//  Created by olivier on 4/24/06.
+//  Copyright 2006 Olivier Destrebecq. All rights reserved.
+//
+
+#import "AbstractConnectionTest.h"
+
+
+@interface fileConnectionTest : FTPConnectionTest 
+
+{
+  
+}
+
+@end
---
+++ unit test/SFTPConnectionTest.h
@@ -0,0 +1,17 @@
+//
+//  SFTPConnectionTest.h
+//  Connection
+//
+//  Created by olivier on 4/23/06.
+//  Copyright 2006 Olivier Destrebecq. All rights reserved.
+//
+
+#import "AbstractConnectionTest.h"
+
+
+@interface SFTPConnectionTest : FTPConnectionTest 
+{
+
+}
+
+@end
