35357221ce05414488ac13fe5f7c352a4433c31d
Olivier Destrebecq <oblivious+odestrebecq@subtlegradient.com>
2006-05-10 06:08:14 +0000

    Fixed issue in file connection class where the filemanager currentdirectory was out of sync with the one from the connection class, added unit test to detect problem

--- unit test/FileConnectionTest.m
+++ unit test/FileConnectionTest.m
@@ -30,4 +30,13 @@
 {
   //NA
 }
+
+- (void) testConnect
+{
+  [super testConnect];
+  
+  //check that the file manager has its path set to the same thing as what the connection returns
+  //
+  STAssertEqualObjects([[connection valueForKey: @"myFileManager"] currentDirectoryPath], [connection currentDirectory], @"path not synchronized between filemanager and connection");
+}
 @end
