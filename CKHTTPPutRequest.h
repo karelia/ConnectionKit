599c90ff3ef038c68d00c307377018696129c867
Greg Hulands <ghulands@me.com>
2006-10-15 01:56:46 +0000

    Expose the file download request

--- CKHTTPPutRequest.h
+++ CKHTTPPutRequest.h
@@ -27,7 +27,7 @@
  WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import "CKHTTPRequest.h"
+#import <Connection/CKHTTPRequest.h>
 
 
 @interface CKHTTPPutRequest : CKHTTPRequest
