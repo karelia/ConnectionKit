9644087789056a9b29bcde25731442a5caa4c63a
Dan Wood <dwood@karelia.com>
2006-12-31 04:34:31 +0000

    Whoops, sorry
    git-svn-id: http://opensource.utr-software.com/source/connection/trunk@341 30aebe3c-6c0d-0410-bd16-8e6c7f3d8b55

---
+++ NSObject+Connection.h
@@ -0,0 +1,15 @@
+//
+//  NSObject+Connection.h
+//  Connection
+//
+//  Created by Dan Wood on 12/30/06.
+//  Copyright (c) 2006 Karelia Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSObject ( Connection )
+
+- (NSString *)shortDescription;
+
+@end
---
+++ NSObject+Connection.m
@@ -0,0 +1,19 @@
+//
+//  NSObject+Connection.m
+//  Connection
+//
+//  Created by Dan Wood on 12/30/06.
+//  Copyright (c) 2006 Karelia Software. All rights reserved.
+//
+
+#import "NSObject+Connection.h"
+
+
+@implementation NSObject ( Connection )
+
+- (NSString *)shortDescription
+{
+	return [self description];
+}
+
+@end
