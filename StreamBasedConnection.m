93174f8194d7180b24bc18263dad91712d3a9601
Dan Wood <dwood@karelia.com>
2007-02-09 05:27:09 +0000

    test before releasing ... but I need to do this check in many more places....

--- StreamBasedConnection.m
+++ StreamBasedConnection.m
@@ -107,7 +107,7 @@ OSStatus SSLWriteFunction(SSLConnectionRef connection, const void *data, size_t 
 
 - (void)dealloc
 {
-	SSLDisposeContext(mySSLContext);
+	if (mySSLContext) SSLDisposeContext(mySSLContext);
 	if (mySSLIdentity) CFRelease(mySSLIdentity);
 	[mySSLSendBuffer release];
 	[mySSLRecevieBuffer release];
