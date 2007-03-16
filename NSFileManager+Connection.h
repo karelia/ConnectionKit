b1c94410df6a96ba475f3e383545dd6d8ea18606
Greg Hulands <ghulands@me.com>
2007-03-16 21:52:49 +0000

    oops

---
+++ NSFileManager+Connection.h
@@ -0,0 +1,16 @@
+//
+//  NSFileManager+Connection.h
+//  Connection
+//
+//  Created by Greg Hulands on 16/03/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface NSFileManager (Connection)
+
+- (void)recursivelyCreateDirectory:(NSString *)path attributes:(NSDictionary *)attributes;
+
+@end
