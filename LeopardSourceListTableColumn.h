ca266296f3829cff6cd8e8992dc3fbda85555b8a
Brian Amerige <brian@extendmac.com>
2008-02-01 03:13:37 +0000

    Bookmark registry now supports a Leopard styled outline view. See http://brianamerige.com/blog/2008/01/31/connectionkit-leopard-styled-source-list-notes/ for more information. CKBonjourCategory updated to automatically refresh outlineview when new hosts go offline and online.

---
+++ LeopardSourceListTableColumn.h
@@ -0,0 +1,15 @@
+//
+//  LeopardSourceListTableColumn.h
+//
+//  Created by brian on 1/26/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface LeopardSourceListTableColumn : NSTableColumn 
+{
+
+}
+
+@end
