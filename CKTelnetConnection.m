91826fd55b2e3e3184a89202a30ca01087519212
Mike Abdullah <me@mikeabdullah.net>
2009-03-22 16:49:58 +0000

    Barebones experimental API for Telnet connections.
    git-svn-id: http://opensource.utr-software.com/source/connection/branches/2.0@1191 30aebe3c-6c0d-0410-bd16-8e6c7f3d8b55

---
+++ CKTelnetConnection.m
@@ -0,0 +1,14 @@
+//
+//  CKTelnetConnection.m
+//  Connection
+//
+//  Created by Mike on 20/03/2009.
+//  Copyright 2009 Karelia Software. All rights reserved.
+//
+
+#import "CKTelnetConnection.h"
+
+
+@implementation CKTelnetConnection
+
+@end
