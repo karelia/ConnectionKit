81d836b65b9793bc7c148f82d3867ed7e3dcc718
Mike Abdullah <me@mikeabdullah.net>
2009-03-26 14:40:34 +0000

    New class CKFTPCommand that takes care of constructing an FTP command in accordance with RFC 959
    git-svn-id: http://opensource.utr-software.com/source/connection/branches/sandvox1.6@1194 30aebe3c-6c0d-0410-bd16-8e6c7f3d8b55

---
+++ CKFTPCommand.m
@@ -0,0 +1,115 @@
+//
+//  CKFTPRequest.m
+//  Connection
+//
+//  Created by Mike on 24/03/2009.
+//  Copyright 2009 Karelia Software. All rights reserved.
+//
+
+#import "CKFTPCommand.h"
+
+
+@implementation CKFTPCommand
+
++ (NSCharacterSet *)FTPCommandCodeCharacters
+{
+    static NSCharacterSet *result = nil;
+    if (!result)
+    {
+        result = [NSCharacterSet characterSetWithCharactersInString:
+                  @"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"];
+        [result retain];
+    }
+    
+    return result;
+}
+
+#pragma mark -
+#pragma mark Init
+
++ (CKFTPCommand *)commandWithCode:(NSString *)code argumentField:(NSString *)argumentField;
+{
+    return [[[self alloc] initWithCommandCode:code argumentField:argumentField] autorelease];
+}
+
++ (CKFTPCommand *)commandWithCode:(NSString *)code
+{
+    return [[[self alloc] initWithCommandCode:code] autorelease];
+}
+
+- (id)initWithCommandCode:(NSString *)verb argumentField:(NSString *)argumentField
+{
+    NSParameterAssert(verb);
+    NSParameterAssert([verb length] > 0 || [verb length] <= 4);
+    
+    NSCharacterSet *legalVerbCharacters = [[self class] FTPCommandCodeCharacters];
+    NSCharacterSet *illegalVerbCharacters = [legalVerbCharacters invertedSet];
+    NSParameterAssert([verb rangeOfCharacterFromSet:illegalVerbCharacters].location == NSNotFound);
+    
+    if (argumentField) NSParameterAssert([argumentField length] > 0);
+    
+    [super init];
+        
+    _commandCode = [verb copy];
+    _argumentField = [argumentField copy];
+    
+    return self;
+}
+
+- (id)initWithCommandCode:(NSString *)code
+{
+    return [self initWithCommandCode:code argumentField:nil];
+}
+
+- (void)dealloc
+{
+    [_commandCode release];
+    [_argumentField release];
+    
+    [super dealloc];
+}
+    
+#pragma mark -
+#pragma mark Accessor Methods
+
+- (NSString *)commandCode { return _commandCode; }
+        
+- (NSString *)argumentField { return _argumentField; }
+
+#pragma mark -
+#pragma mark Copy
+
+- (id)copyWithZone:(NSZone *)zone
+{
+    return [self retain];   // immutable object
+}
+
+#pragma mark -
+#pragma mark Description and Serialization
+
+- (NSData *)serializedCommand
+{
+    NSString *resultString = [[self serializedTelnetString] stringByAppendingString:@"\r\n"];
+    NSData *result = [resultString dataUsingEncoding:NSASCIIStringEncoding];
+    return result;
+}
+
+- (NSString *)serializedTelnetString
+{
+    NSString *result = [[self commandCode] uppercaseString];
+    NSString *parameter = [self argumentField];
+    if (parameter)
+    {
+        result = [result stringByAppendingFormat:@" %@", parameter];
+    }
+    
+    return result;
+}
+
+- (NSString *)description
+{
+    NSString *result = [[super description] stringByAppendingString:[self serializedTelnetString]];
+    return result;
+}
+
+@end
