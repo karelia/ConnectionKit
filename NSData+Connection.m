17d04d4477f5f333e8390ee525cfd16bdc4951d9
Mike Abdullah <me@mikeabdullah.net>
2009-04-15 17:17:05 +0000

    Correcting spacing on NSData methods. Removed -sha1Digest as it's not in use.

--- NSData+Connection.m
+++ NSData+Connection.m
@@ -259,17 +259,6 @@
 	return [NSData dataWithBytes:md_value length:md_len];
 }
 
-- (NSData *)sha1Digest
-{
-	EVP_MD_CTX mdctx;
-	unsigned char md_value[EVP_MAX_MD_SIZE];
-	unsigned int md_len;
-	EVP_DigestInit(&mdctx, EVP_sha1());
-	EVP_DigestUpdate(&mdctx, [self bytes], [self length]);
-	EVP_DigestFinal(&mdctx, md_value, &md_len);
-	return [NSData dataWithBytes:md_value length:md_len];
-}
-
 - (NSData *)sha1HMacWithKey:(NSString*)key
 {
 	HMAC_CTX mdctx;
