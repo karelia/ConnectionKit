53c6119444847f3ddb990efcaab13bacac961846
Brian Amerige <brian@extendmac.com>
2009-06-19 01:01:56 +0000

    Removed the many unncessary methods of uploading. Replaced with one single method to handle both file and directory uploads. Data uploading currently unimplemented. Reorganized CKTransferRecord.
    git-svn-id: http://opensource.utr-software.com/source/connection/branches/BrianWorkInProgress@1262 30aebe3c-6c0d-0410-bd16-8e6c7f3d8b55

--- CKTransferRecord.h
+++ CKTransferRecord.h
@@ -1,50 +1,20 @@
-/*
- Copyright (c) 2006, Greg Hulands <ghulands@mac.com>
- All rights reserved.
- 
- Redistribution and use in source and binary forms, with or without modification, 
- are permitted provided that the following conditions are met:
- 
- Redistributions of source code must retain the above copyright notice, this list 
- of conditions and the following disclaimer.
- 
- Redistributions in binary form must reproduce the above copyright notice, this 
- list of conditions and the following disclaimer in the documentation and/or other 
- materials provided with the distribution.
- 
- Neither the name of Greg Hulands nor the names of its contributors may be used to 
- endorse or promote products derived from this software without specific prior 
- written permission.
- 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
- EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
- OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
- SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
- INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
- BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY 
- WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-
 @protocol CKConnection;
-
 
 @interface CKTransferRecord : NSObject
 {
 	BOOL _isUpload;
 	NSString *_name;
-	unsigned long long _size;
-	unsigned long long _sizeWithChildren;
-	unsigned long long _transferred;
-	unsigned long long _intermediateTransferred;
+	unsigned long long _sizeInBytes;
+	unsigned long long _sizeInBytesWithChildren;
+	unsigned long long _numberOfBytesTransferred;
+	unsigned long long _numberOfBytesInLastTransferChunk;
+
 	NSTimeInterval _lastTransferTime;
 	NSTimeInterval _transferStartTime;
 	NSTimeInterval _lastDirectorySpeedUpdate;
 	float _speed;
 	NSUInteger _progress;
-	NSMutableArray *_contents;
+	NSMutableArray *_children;
 	CKTransferRecord *_parent; //not retained
 	NSMutableDictionary *_properties;
 	
@@ -86,6 +56,7 @@
 - (NSArray *)contents;
 
 - (BOOL)hasError;
+- (BOOL)problemsTransferringCountingErrors:(int *)outErrors successes:(int *)outSuccesses;
 
 - (CKTransferRecord *)root;
 - (NSString *)path; 
@@ -97,21 +68,6 @@
 - (void)setObject:(id)object forKey:(id)key;
 - (id)objectForKey:(id)key;
 
-// Helper methods for working with the recursive data structure
-
-+ (CKTransferRecord *)rootRecordWithPath:(NSString *)path;
-+ (CKTransferRecord *)addFileRecord:(NSString *)file 
-							   size:(unsigned long long)size 
-						   withRoot:(CKTransferRecord *)root 
-						   rootPath:(NSString *)rootPath;
-+ (CKTransferRecord *)recursiveRecord:(CKTransferRecord *)record forFullPath:(NSString *)path;
-+ (CKTransferRecord *)recordForFullPath:(NSString *)path withRoot:(CKTransferRecord *)root;
-+ (CKTransferRecord *)recursiveRecord:(CKTransferRecord *)record forPath:(NSString *)path;
-+ (CKTransferRecord *)recordForPath:(NSString *)path withRoot:(CKTransferRecord *)root;
-+ (void)mergeRecord:(CKTransferRecord *)record withRoot:(CKTransferRecord *)root;
-+ (void)mergeTextPathRecord:(CKTransferRecord *)record withRoot:(CKTransferRecord *)root;
-
-- (BOOL)problemsTransferringCountingErrors:(int *)outErrors successes:(int *)outSuccesses;
 
 @end
 
