40dfde7d4146582f4c17ed891e2e61e3656f0ca8
Brian Amerige <brian@extendmac.com>
2009-09-07 23:27:18 +0000

    Removed 10.4 code, and CKLeopardSourceListTableColumn header from Connection.h import.
    git-svn-id: http://opensource.utr-software.com/source/connection/branches/BrianWorkInProgress@1435 30aebe3c-6c0d-0410-bd16-8e6c7f3d8b55

--- Connection.h
+++ Connection.h
@@ -27,26 +27,6 @@
  WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-
-// For Mac OS X < 10.5.
-#ifndef NSINTEGER_DEFINED
-#define NSINTEGER_DEFINED
-#ifdef __LP64__ || NS_BUILD_32_LIKE_64
-typedef long           NSInteger;
-typedef unsigned long  NSUInteger;
-#define NSIntegerMin   LONG_MIN
-#define NSIntegerMax   LONG_MAX
-#define NSUIntegerMax  ULONG_MAX
-#else
-typedef int            NSInteger;
-typedef unsigned int   NSUInteger;
-#define NSIntegerMin   INT_MIN
-#define NSIntegerMax   INT_MAX
-#define NSUIntegerMax  UINT_MAX
-#endif
-#endif // NSINTEGER_DEFINED
-
-
 #import <Cocoa/Cocoa.h>
 
 #import <Connection/CKConnectionProtocol.h>
@@ -81,5 +61,3 @@ typedef unsigned int   NSUInteger;
 #import <Connection/CKHTTPPutRequest.h>
 #import <Connection/CKHTTPResponse.h>
 #import <Connection/CKHTTPFileDownloadResponse.h>
-
-#import <Connection/CKLeopardSourceListTableColumn.h>
