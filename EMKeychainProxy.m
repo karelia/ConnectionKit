e27092326bc87c9122b04f6ad33125e9a4967cc8
Mike Abdullah <me@mikeabdullah.net>
2011-06-29 12:35:40 +0100

    +stringWithCString:length: seems to be a necessary evil to retrieve passwords from keychain correctly for now

--- EMKeychainProxy.m
+++ EMKeychainProxy.m
@@ -148,7 +148,7 @@ static EMKeychainProxy *sharedProxy = nil;
 		}
 		return nil;
 	}
-	NSString *passwordString = [NSString stringWithCharacters:password length:passwordLength];
+	NSString *passwordString = [NSString stringWithCString:password length:passwordLength];
 	SecKeychainItemFreeContent(NULL, password);
 	
 	return [EMInternetKeychainItem internetKeychainItem:item forServer:serverString username:usernameString password:passwordString path:pathString port:port protocol:protocol];
