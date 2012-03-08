07b2af5724898d74effae9a98a4e8d127ed8e957
Sam Deane <sam@elegantchaos.com>
2012-03-08 16:17:51 +0000

    added HTTP tests

--- unit test/PathTests.m
+++ unit test/PathTests.m
@@ -16,32 +16,60 @@
 
 @implementation PathTests
 
-- (void)testEmptyNoTrailingSlash
+- (void)testFTPEmptyNoTrailingSlash
 {
     NSURL* testURL = [NSURL URLWithString:@"ftp://test.ftp.com"];
     NSString* path = [[CKConnectionRegistry sharedConnectionRegistry] pathOfURLRelativeToHomeDirectory:testURL];
     STAssertTrue([path length] == 0, @"path should be empty");
 }
 
-- (void)testEmptyTrailingSlash
+- (void)testFTPEmptyTrailingSlash
 {
     NSURL* testURL = [NSURL URLWithString:@"ftp://test.ftp.com/"];
     NSString* path = [[CKConnectionRegistry sharedConnectionRegistry] pathOfURLRelativeToHomeDirectory:testURL];
     STAssertTrue([path length] == 0, @"path should be empty");
 }
 
-- (void)testRelative
+- (void)testFTPRelative
 {
     NSURL* testURL = [NSURL URLWithString:@"ftp://test.ftp.com/relative/path/file.txt"];
     NSString* path = [[CKConnectionRegistry sharedConnectionRegistry] pathOfURLRelativeToHomeDirectory:testURL];
     STAssertTrue([path isEqualToString:@"relative/path/file.txt"], @"path shouldn't start with slash");
 }
 
-- (void)testAlsolute
+- (void)testFTPAlsolute
 {
     NSURL* testURL = [NSURL URLWithString:@"ftp://test.ftp.com//absolute/path/file.txt"];
     NSString* path = [[CKConnectionRegistry sharedConnectionRegistry] pathOfURLRelativeToHomeDirectory:testURL];
     STAssertTrue([path isEqualToString:@"/absolute/path/file.txt"], @"path should start with slash");
+}
+
+- (void)testHTTPEmptyNoTrailingSlash
+{
+    NSURL* testURL = [NSURL URLWithString:@"http://test.ftp.com"];
+    NSString* path = [[CKConnectionRegistry sharedConnectionRegistry] pathOfURLRelativeToHomeDirectory:testURL];
+    STAssertTrue([path length] == 0, @"path should be empty");
+}
+
+- (void)testHTTPEmptyTrailingSlash
+{
+    NSURL* testURL = [NSURL URLWithString:@"http://test.ftp.com/"];
+    NSString* path = [[CKConnectionRegistry sharedConnectionRegistry] pathOfURLRelativeToHomeDirectory:testURL];
+    STAssertTrue([path isEqualToString:@"/"], @"path should be /");
+}
+
+- (void)testHTTPRelative
+{
+    NSURL* testURL = [NSURL URLWithString:@"http://test.ftp.com/relative/path/file.txt"];
+    NSString* path = [[CKConnectionRegistry sharedConnectionRegistry] pathOfURLRelativeToHomeDirectory:testURL];
+    STAssertTrue([path isEqualToString:@"/relative/path/file.txt"], @"path should be absolute from root");
+}
+
+- (void)testHTTPAlsolute
+{
+    NSURL* testURL = [NSURL URLWithString:@"http://test.ftp.com//absolute/path/file.txt"];
+    NSString* path = [[CKConnectionRegistry sharedConnectionRegistry] pathOfURLRelativeToHomeDirectory:testURL];
+    STAssertTrue([path isEqualToString:@"//absolute/path/file.txt"], @"path should start with double slash");
 }
 
 @end
