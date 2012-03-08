3d70aadc7440954ea8006e47c2009d9fdf6add88
Sam Deane <sam@elegantchaos.com>
2012-03-08 15:17:43 +0000

    added new unit tests

---
+++ unit test/Tests.h
@@ -0,0 +1,13 @@
+//
+//  Tests.h
+//  Tests
+//
+//  Created by Sam Deane on 08/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Tests : SenTestCase
+
+@end
---
+++ unit test/Tests.m
@@ -0,0 +1,32 @@
+//
+//  Tests.m
+//  Tests
+//
+//  Created by Sam Deane on 08/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Tests.h"
+
+@implementation Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Tests");
+}
+
+@end
