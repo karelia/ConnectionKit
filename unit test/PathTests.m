d42e1e0a6cbf329c6d6c2861e01b5993f0778b63
Sam Deane <sam@elegantchaos.com>
2012-03-08 16:04:20 +0000

    filled in ftp path tests

---
+++ unit test/PathTests.m
@@ -0,0 +1,47 @@
+//
+//  Tests.m
+//  Tests
+//
+//  Created by Sam Deane on 08/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CKConnectionRegistry.h"
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PathTests : SenTestCase
+
+@end
+
+@implementation PathTests
+
+- (void)testEmptyNoTrailingSlash
+{
+    NSURL* testURL = [NSURL URLWithString:@"ftp://test.ftp.com"];
+    NSString* path = [[CKConnectionRegistry sharedConnectionRegistry] pathOfURLRelativeToHomeDirectory:testURL];
+    STAssertTrue([path isEqualToString:@""], @"path should be empty");
+}
+
+- (void)testEmptyTrailingSlash
+{
+    NSURL* testURL = [NSURL URLWithString:@"ftp://test.ftp.com/"];
+    NSString* path = [[CKConnectionRegistry sharedConnectionRegistry] pathOfURLRelativeToHomeDirectory:testURL];
+    STAssertTrue([path isEqualToString:@""], @"path should be empty");
+}
+
+- (void)testRelative
+{
+    NSURL* testURL = [NSURL URLWithString:@"ftp://test.ftp.com/relative/path/file.txt"];
+    NSString* path = [[CKConnectionRegistry sharedConnectionRegistry] pathOfURLRelativeToHomeDirectory:testURL];
+    STAssertTrue([path isEqualToString:@"relative/path/file.txt"], @"path shouldn't start with slash");
+}
+
+- (void)testAlsolute
+{
+    NSURL* testURL = [NSURL URLWithString:@"ftp://test.ftp.com//absolute/path/file.txt"];
+    NSString* path = [[CKConnectionRegistry sharedConnectionRegistry] pathOfURLRelativeToHomeDirectory:testURL];
+    STAssertTrue([path isEqualToString:@"/absolute/path/file.txt"], @"path should start with slash");
+}
+
+@end
