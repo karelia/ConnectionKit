1e832e3d1fa4d8c9df09a518ecd26bab1ef52253
Mike Abdullah <me@mikeabdullah.net>
2012-10-15 10:45:51 +0100

    tweak naming

--- CK2FileTransferProtocol.h
+++ CK2FileTransferProtocol.h
@@ -74,7 +74,7 @@
 + (void)registerClass:(Class)protocolClass;
 
 // Completion block is guaranteed to be called on our private serial queue
-+ (void)classForURL:(NSURL *)url completionHandler:(void (^)(Class protocol))block;
++ (void)classForURL:(NSURL *)url completionHandler:(void (^)(Class protocolClass))block;
 
 + (Class)classForURL:(NSURL *)url;    // only suitable for stateless calls to the protocol class
 
