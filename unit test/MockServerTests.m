08bd228524c4cdabaa3a9440fc5f76bab16333be
Sam Deane <sam@elegantchaos.com>
2012-11-06 18:01:44 +0000

    added HEAD test

--- unit test/MockServerTests.m
+++ unit test/MockServerTests.m
@@ -15,46 +15,74 @@
 
 @implementation MockServerTests
 
-#pragma mark - Tests
+static NSString *const HTTPHeader = @"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=iso-8859-1\r\n\r\n";
+static NSString*const HTTPContent = @"<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\"><html><head><title>example</title></head><body>example result</body></html>\n";
 
-- (void)testServer
+- (MockServer*)setupServer
 {
-    NSString* expectedRequest = @"^GET .* HTTP.*";
-    NSString* expectedResponse = @"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=iso-8859-1\r\n\r\n";
-    NSString* expectedContent = @"<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\"><html><head><title>example</title></head><body>example result</body></html>\n";
-
     id closeCommand = [MockServer closeResponse];
 
-    MockServer* server = [MockServer serverWithPort:0 responses:@{ expectedRequest : @[ expectedResponse, expectedContent, @(0.1), closeCommand] }];
-    
+    MockServer* server = [MockServer serverWithPort:0 responses:@{
+                          @"^GET .* HTTP.*" : @[ HTTPHeader, HTTPContent, @(0.1), closeCommand],
+                          @"^HEAD .* HTTP.*" : @[ HTTPHeader, closeCommand],
+                          }];
+
     STAssertNotNil(server, @"got server");
     [server start];
     BOOL started = server.running;
     STAssertTrue(started, @"server started ok");
+    return started ? server : nil;
+}
 
-    if (started)
-    {
-        NSURL* url = [NSURL URLWithString:[NSString stringWithFormat:@"http://127.0.0.1:%ld/index.html", (long)server.port]];
+- (NSString*)stringForScheme:(NSString*)scheme path:(NSString*)path method:(NSString*)method server:(MockServer*)server
+{
+    NSURL* url = [NSURL URLWithString:[NSString stringWithFormat:@"%@://127.0.0.1:%ld%@", scheme, (long)server.port, path]];
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = method;
 
-        __block NSString* string = nil;
+    __block NSString* string = nil;
 
-        [NSURLConnection sendAsynchronousRequest:[NSURLRequest requestWithURL:url] queue:[NSOperationQueue currentQueue] completionHandler:^(NSURLResponse* response, NSData* data, NSError* error)
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue currentQueue] completionHandler:^(NSURLResponse* response, NSData* data, NSError* error)
+     {
+         if (error)
          {
-             if (error)
-             {
-                 NSLog(@"got error %@", error);
-             }
-             else
-             {
-                 string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-             }
+             NSLog(@"got error %@", error);
+         }
+         else
+         {
+             string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+         }
 
-             [server stop];
-         }];
+         [server stop];
+     }];
 
-        [server runUntilStopped];
-        
-        STAssertEqualObjects(string, expectedContent, @"wrong response");
+    [server runUntilStopped];
+
+    return string;
+}
+
+#pragma mark - Tests
+
+- (void)testHTTPGet
+{
+    MockServer* server = [self setupServer];
+    if (server)
+    {
+        NSString* string = [self stringForScheme:@"http" path:@"/index.html" method:@"GET" server:server];
+
+        STAssertEqualObjects(string, HTTPContent, @"wrong response");
+        [string release];
+    }
+}
+
+- (void)testHTTPHead
+{
+    MockServer* server = [self setupServer];
+    if (server)
+    {
+        NSString* string = [self stringForScheme:@"http" path:@"/index.html" method:@"HEAD" server:server];
+
+        STAssertEqualObjects(string, @"", @"wrong response");
         [string release];
     }
 }
