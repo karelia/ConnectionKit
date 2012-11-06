0f9375fc8c4dbd9b4b6d3b834747332168f55e46
Sam Deane <sam@elegantchaos.com>
2012-11-06 14:06:58 +0000

    got basic reading/writing working

--- unit test/MockServer.m
+++ unit test/MockServer.m
@@ -12,6 +12,9 @@
 #import <sys/socket.h>
 #import <netinet/in.h>   // for IPPROTO_TCP, sockaddr_in
 
+#define MockServerLog NSLog
+#define MockServerAssert(x) assert((x))
+
 @interface ECTestServer : NSObject<NSStreamDelegate>
 {
     NSUInteger _port;
@@ -28,13 +31,11 @@
 
 @property (assign, nonatomic) NSUInteger port;
 @property (strong, nonatomic) NSDictionary* responses;
-@property (assign, nonatomic) dispatch_queue_t queue;
+@property (strong, nonatomic) NSMutableData* outputData;
 
-
-@property (nonatomic, assign, readonly ) BOOL               isStarted;
-@property (nonatomic, assign, readonly ) BOOL               isReceiving;
 @property (nonatomic, assign, readwrite) CFSocketRef        listeningSocket;
-@property (nonatomic, strong, readwrite) NSInputStream *    networkStream;
+@property (nonatomic, strong, readwrite) NSInputStream *    input;
+@property (nonatomic, strong, readwrite) NSOutputStream *   output;
 
 @end
 
@@ -42,9 +43,9 @@
 
 @synthesize port = _port;
 @synthesize responses = _responses;
-@synthesize queue = _queue;
-
-@synthesize networkStream   = _networkStream;
+@synthesize outputData = _outputData;
+@synthesize input   = _input;
+@synthesize output = _output;
 @synthesize listeningSocket = _listeningSocket;
 
 + (ECTestServer*)serverWithPort:(NSUInteger)port responses:(NSDictionary*)responses
@@ -60,8 +61,8 @@
     {
         self.port = port;
         self.responses = responses;
-        self.queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-
+        self.outputData = [NSMutableData data];
+        MockServerLog(@"made server at port %ld", (long) port);
     }
 
     return self;
@@ -69,90 +70,176 @@
 
 - (void)dealloc
 {
-    dispatch_release(self.queue);
+    [_input release];
+    [_output release];
+    [_outputData release];
 
     [super dealloc];
 }
 
-- (BOOL)isReceiving
-{
-    return (self.networkStream != nil);
-}
-
 - (void)startReceive:(int)fd
 {
-    CFReadStreamRef     readStream;
+    MockServerLog(@"received connection");
 
-    assert(fd >= 0);
-    assert(self.networkStream == nil);      // can't already be receiving
+    CFReadStreamRef     readStream;
+    CFWriteStreamRef    writeStream;
+
+    MockServerAssert(fd >= 0);
+    MockServerAssert(self.input == nil);      // can't already be receiving
+    MockServerAssert(self.output == nil);      // can't already be receiving
 
     // Open a stream based on the existing socket file descriptor.  Then configure
     // the stream for async operation.
 
-    CFStreamCreatePairWithSocket(NULL, fd, &readStream, NULL);
-    assert(readStream != NULL);
+    CFStreamCreatePairWithSocket(NULL, fd, &readStream, &writeStream);
+    MockServerAssert(readStream);
+    MockServerAssert(writeStream);
 
-    self.networkStream = ( NSInputStream *) readStream;
+    self.input = (NSInputStream*)readStream;
+    self.output = (NSOutputStream*)writeStream;
 
     CFRelease(readStream);
+    CFRelease(writeStream);
 
-    [self.networkStream setProperty:(id)kCFBooleanTrue forKey:(NSString *)kCFStreamPropertyShouldCloseNativeSocket];
+    [self.input setProperty:(id)kCFBooleanTrue forKey:(NSString *)kCFStreamPropertyShouldCloseNativeSocket];
+    [self.output setProperty:(id)kCFBooleanTrue forKey:(NSString *)kCFStreamPropertyShouldCloseNativeSocket];
 
-    self.networkStream.delegate = self;
-    [self.networkStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    self.input.delegate = self;
+    self.output.delegate = self;
 
-    [self.networkStream open];
+    [self.input scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    [self.output scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+
+    [self.input open];
+    [self.output open];
 }
 
-- (void)stopReceiveWithStatus:(NSString *)statusString
+- (void)disconnect:(NSString*)reason
 {
-    if (self.networkStream != nil) {
-        self.networkStream.delegate = nil;
-        [self.networkStream removeFromRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-        [self.networkStream close];
-        self.networkStream = nil;
+    if (self.input != nil)
+    {
+        self.input.delegate = nil;
+        [self.input removeFromRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+        [self.input close];
+        self.input = nil;
     }
+
+    if (self.output != nil)
+    {
+        self.output.delegate = nil;
+        [self.output removeFromRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+        [self.output close];
+        self.output = nil;
+    }
+
+    MockServerLog(@"disconnected: %@", reason);
+}
+
+- (NSString*)nameForStream:(NSStream*)stream
+{
+    NSString* result;
+    if (stream == self.input)
+    {
+        result = @"input";
+    }
+    else if (stream == self.output)
+    {
+        result = @"output";
+    }
+    else
+    {
+        result = @"unknown";
+    }
+
+    return result;
 }
 
 - (void)stream:(NSStream *)aStream handleEvent:(NSStreamEvent)eventCode
 // An NSStream delegate callback that's called when events happen on our
 // network stream.
 {
-    assert(aStream == self.networkStream);
-#pragma unused(aStream)
+    MockServerAssert((aStream == self.input) || (aStream == self.output));
 
-    switch (eventCode) {
-        case NSStreamEventOpenCompleted: {
-        } break;
-        case NSStreamEventHasBytesAvailable: {
+    switch (eventCode)
+    {
+        case NSStreamEventOpenCompleted:
+        {
+            MockServerLog(@"opened %@ stream", [self nameForStream:aStream]);
+            break;
+        }
+
+        case NSStreamEventHasBytesAvailable:
+        {
             NSInteger       bytesRead;
             uint8_t         buffer[32768];
 
             // Pull some data off the network.
 
-            bytesRead = [self.networkStream read:buffer maxLength:sizeof(buffer)];
-            if (bytesRead == -1) {
-                [self stopReceiveWithStatus:@"Network read error"];
-            } else if (bytesRead == 0) {
-                [self stopReceiveWithStatus:nil];
-            } else {
-                NSLog(@"read bytes %ld", bytesRead);
-                // TODO: write response
+            bytesRead = [self.input read:buffer maxLength:sizeof(buffer)];
+            if (bytesRead == -1)
+            {
+                [self disconnect:@"Network read error"];
             }
-        } break;
-        case NSStreamEventHasSpaceAvailable: {
-            assert(NO);     // should never happen for the output stream
-        } break;
-        case NSStreamEventErrorOccurred: {
-            [self stopReceiveWithStatus:@"Stream open error"];
-        } break;
-        case NSStreamEventEndEncountered: {
-            // ignore
-        } break;
-        default: {
-            assert(NO);
-        } break;
+
+            else if (bytesRead == 0)
+            {
+                [self disconnect:@"No more data"];
+            }
+            else
+            {
+                NSData* data = [NSData dataWithBytesNoCopy:buffer length:bytesRead];
+                [self processInput:data];
+            }
+            break;
+        }
+
+        case NSStreamEventHasSpaceAvailable:
+        {
+            MockServerAssert(aStream == self.output);     // should never happen for the input stream
+            NSUInteger bytesToWrite = [self.outputData length];
+            if (bytesToWrite)
+            {
+                NSUInteger written = [self.output write:[self.outputData bytes] maxLength:bytesToWrite];
+                [self.outputData replaceBytesInRange:NSMakeRange(0, written) withBytes:nil length:0];
+
+                MockServerLog(@"wrote %ld bytes", (long)written);
+            }
+            else
+            {
+                MockServerLog(@"nothing to write");
+            }
+            break;
+        }
+
+        case NSStreamEventErrorOccurred:
+        {
+            MockServerLog(@"got error for %@ stream", [self nameForStream:aStream]);
+            [self disconnect:@"Stream open error"];
+            break;
+        }
+
+        case NSStreamEventEndEncountered:
+        {
+            MockServerLog(@"got eof for %@ stream", [self nameForStream:aStream]);
+            break;
+        }
+
+        default:
+        {
+            MockServerLog(@"unknown event for %@ stream", [self nameForStream:aStream]);
+            MockServerAssert(NO);
+            break;
+        }
     }
+}
+
+- (void)processInput:(NSData*)data
+{
+    NSString* string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    MockServerLog(@"got data %@", string);
+
+    NSString* output = @"<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">\n<html><head>\n<title>400 Bad Request</title>\n</head><body>\n<h1>Bad Request</h1>\n<p>Your browser sent a request that this server could not understand.<br />\n</p>\n<hr>\n<address>Apache/2.2.14 (Ubuntu) Server at downloads.elegantchaos.com Port 80</address>\n</body></html>";
+    [self.outputData appendData:[output dataUsingEncoding:NSUTF8StringEncoding]];
 }
 
 - (void)acceptConnection:(int)fd
@@ -163,9 +250,10 @@
     // big simplifying assumptions in this code.  A real server should handle
     // multiple simultaneous connections.
 
-    if ( self.isReceiving ) {
+    if (self.input)
+    {
         junk = close(fd);
-        assert(junk == 0);
+        MockServerAssert(junk == 0);
     } else {
         [self startReceive:fd];
     }
@@ -178,109 +266,180 @@ static void AcceptCallback(CFSocketRef s, CFSocketCallBackType type, CFDataRef a
     ECTestServer *  obj;
 
 #pragma unused(type)
-    assert(type == kCFSocketAcceptCallBack);
+    MockServerAssert(type == kCFSocketAcceptCallBack);
 #pragma unused(address)
-    // assert(address == NULL);
-    assert(data != NULL);
+    // MockServerAssert(address == NULL);
+    MockServerAssert(data);
 
     obj = ( ECTestServer *) info;
-    assert(obj != nil);
+    MockServerAssert(obj != nil);
 
-    assert(s == obj->_listeningSocket);
+    MockServerAssert(s == obj->_listeningSocket);
 #pragma unused(s)
 
     [obj acceptConnection:*(int *)data];
 }
 
+- (BOOL)makeSocket:(int*)socketOut
+{
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketOut)
+        *socketOut = fd;
+
+    BOOL result = (fd != -1);
+
+    if (result)
+    {
+        MockServerLog(@"got socket %d", fd);
+    }
+    else
+    {
+        MockServerLog(@"couldn't make socket");
+    }
+
+    return result;
+}
+
+- (BOOL)bindSocket:(int)socket
+{
+    struct sockaddr_in  addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_len    = sizeof(addr);
+    addr.sin_family = AF_INET;
+    addr.sin_port   = htons(self.port);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    int err = bind(socket, (const struct sockaddr *) &addr, sizeof(addr));
+    BOOL result = (err == 0);
+    if (!result)
+    {
+        MockServerLog(@"couldn't bind socket %d, error %d", socket, err);
+    }
+    
+    return result;
+}
+
+- (BOOL)listenOnSocket:(int)socket
+{
+    int err = listen(socket, 5);
+    BOOL result = err == 0;
+
+    if (!result)
+    {
+        MockServerLog(@"couldn't listen on socket %d", socket);
+    }
+
+    return result;
+}
+
+- (BOOL)retrievePortForSocket:(int)socket
+{
+    // If we bound to port 0 the kernel will have assigned us a port.
+    // use getsockname to find out what port number we actually got.
+    struct sockaddr_in  addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_len    = sizeof(addr);
+    addr.sin_family = AF_INET;
+    addr.sin_port   = htons(self.port);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    socklen_t addrLen = sizeof(addr);
+    int err = getsockname(socket, (struct sockaddr *) &addr, &addrLen);
+    BOOL result = (err == 0);
+    if (result)
+    {
+        MockServerAssert(addrLen == sizeof(addr));
+        self.port = ntohs(addr.sin_port);
+    }
+    else
+    {
+        MockServerLog(@"couldn't retrieve socket port");
+    }
+
+    return result;
+}
+
+- (BOOL)makeCFSocketForSocket:(int)socket
+{
+    // Use CFSocket to integrate the socket into our runloop.
+
+    CFSocketContext context = { 0, (void *) self, NULL, NULL, NULL };
+
+    MockServerAssert(self->_listeningSocket == NULL);
+    self->_listeningSocket = CFSocketCreateWithNative(NULL, socket, kCFSocketAcceptCallBack, AcceptCallback, &context);
+
+    BOOL result = (self->_listeningSocket != nil);
+
+    if (result)
+    {
+        CFRunLoopSourceRef  rls;
+
+        rls = CFSocketCreateRunLoopSource(NULL, self.listeningSocket, 0);
+        MockServerAssert(rls);
+
+        CFRunLoopAddSource(CFRunLoopGetCurrent(), rls, kCFRunLoopDefaultMode);
+
+        CFRelease(rls);
+    }
+    else
+    {
+        MockServerLog(@"couldn't make CFSocket for socket %d", socket);
+    }
+
+    return result;
+}
+
 - (void)startServer
 {
-    BOOL                success;
-    int                 err;
-    int                 fd;
-    int                 junk;
-    struct sockaddr_in  addr;
+    int socket;
 
-    // Create a listening socket and use CFSocket to integrate it into our
-    // runloop.  We bind to port 0, which causes the kernel to give us
-    // any free port, then use getsockname to find out what port number we
-    // actually got.
-
-    fd = socket(AF_INET, SOCK_STREAM, 0);
-    success = (fd != -1);
-
-    if (success) {
-        memset(&addr, 0, sizeof(addr));
-        addr.sin_len    = sizeof(addr);
-        addr.sin_family = AF_INET;
-        addr.sin_port   = htons(self.port);
-        addr.sin_addr.s_addr = INADDR_ANY;
-        err = bind(fd, (const struct sockaddr *) &addr, sizeof(addr));
-        success = (err == 0);
-    }
-    if (success) {
-        err = listen(fd, 5);
-        success = (err == 0);
-    }
-    if (success) {
-        socklen_t   addrLen;
-
-        addrLen = sizeof(addr);
-        err = getsockname(fd, (struct sockaddr *) &addr, &addrLen);
-        success = (err == 0);
-
-        if (success) {
-            assert(addrLen == sizeof(addr));
-            self.port = ntohs(addr.sin_port);
-        }
-    }
-    if (success) {
-        CFSocketContext context = { 0, (void *) self, NULL, NULL, NULL };
-
-        assert(self->_listeningSocket == NULL);
-        self->_listeningSocket = CFSocketCreateWithNative(
-                                                          NULL,
-                                                          fd,
-                                                          kCFSocketAcceptCallBack,
-                                                          AcceptCallback,
-                                                          &context
-                                                          );
-        success = (self->_listeningSocket != NULL);
-
-        if (success) {
-            CFRunLoopSourceRef  rls;
-
-            fd = -1;        // listeningSocket is now responsible for closing fd
-
-            rls = CFSocketCreateRunLoopSource(NULL, self.listeningSocket, 0);
-            assert(rls != NULL);
-
-            CFRunLoopAddSource(CFRunLoopGetCurrent(), rls, kCFRunLoopDefaultMode);
-
-            CFRelease(rls);
-        }
+    BOOL success = [self makeSocket:&socket];
+    if (success)
+    {
+        success = [self bindSocket:socket];
     }
 
+    if (success)
+    {
+        success = [self listenOnSocket:socket];
+    }
+    
+    if (success && (self.port == 0))
+    {
+        success = [self retrievePortForSocket:socket];
+    }
 
-    // Clean up after failure.
+    if (success)
+    {
+        success = [self makeCFSocketForSocket:socket];
+    }
 
-    if ( success ) {
-        assert(self.port != 0);
+    if (success)
+    {
+        MockServerAssert(self.port != 0);
         NSLog(@"server started on port %ld", self.port);
-    } else {
+    }
+    else
+    {
         [self stopServer:@"Start failed"];
-        if (fd != -1) {
-            junk = close(fd);
-            assert(junk == 0);
+        if (socket != -1)
+        {
+            int err = close(socket);
+            if (!err)
+            {
+                MockServerLog(@"couldn't close socket %d", socket);
+            }
         }
     }
 }
 
 - (void)stopServer:(NSString *)reason
 {
-    if (self.isReceiving) {
-        [self stopReceiveWithStatus:@"Cancelled"];
+    if (self.input)
+    {
+        [self disconnect:@"Cancelled"];
     }
-    if (self.listeningSocket != NULL) {
+
+    if (self.listeningSocket)
+    {
         CFSocketInvalidate(self.listeningSocket);
         CFRelease(self->_listeningSocket);
         self->_listeningSocket = NULL;
@@ -336,7 +495,7 @@ static void AcceptCallback(CFSocketRef s, CFSocketCallBackType type, CFDataRef a
 
     __block NSString* string = nil;
 
-    [NSURLConnection sendAsynchronousRequest:[NSURLRequest requestWithURL:url] queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse* response, NSData* data, NSError* error)
+    [NSURLConnection sendAsynchronousRequest:[NSURLRequest requestWithURL:url] queue:[NSOperationQueue currentQueue] completionHandler:^(NSURLResponse* response, NSData* data, NSError* error)
     {
         if (error)
         {
