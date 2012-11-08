a41f96c6bd30b04a1e6007e985e5c9fb9c30d998
Sam Deane <sam@elegantchaos.com>
2012-11-08 12:35:37 +0000

    added test for enumerating directory

--- unit test/CK2FileTransferSessionTests.m
+++ unit test/CK2FileTransferSessionTests.m
@@ -13,16 +13,18 @@
 
 @implementation CK2FileTransferSessionTests
 
+static NSString *const ExampleListing = @"total 1\r\n-rw-------   1 user  staff     3 Mar  6  2012 file1.txt\r\n-rw-------   1 user  staff     3 Mar  6  2012 file2.txt\r\n\r\n";
+
 - (NSArray*)ftpResponses
 {
     NSArray* responses = @[
     @[InitialResponseKey, @"220 $address FTP server ($server) ready.\r\n" ],
-    @[@"USER user", @"331 User user accepted, provide password.\r\n"],
-    @[@"PASS pass", @"230 User user logged in.\r\n"],
+    @[@"USER (\\w+)", @"331 User $1 accepted, provide password.\r\n"],
+    @[@"PASS (\\w+)", @"230 User user logged in.\r\n"],
     @[@"SYST", @"215 UNIX Type: L8 Version: $server\r\n" ],
     @[@"PWD", @"257 \"/\" is the current directory.\r\n" ],
     @[@"TYPE (\\w+)", @"200 Type set to $1.\r\n" ],
-    @[@"CWD directory", @"250 CWD command successful.\r\n" ],
+    @[@"CWD .*", @"250 CWD command successful.\r\n" ],
     @[@"PASV", @"227 Entering Passive Mode ($pasv)\r\n"],
     @[@"SIZE test.txt", @"213 $size\r\n"],
     @[@"RETR /test.txt", @"150 Opening BINARY mode data connection for '/test.txt' ($size bytes).\r\n"],
@@ -59,17 +61,22 @@
 
 #pragma mark - Tests
 
-- (void)testFTP
+- (NSURL*)URLForPath:(NSString*)path server:(MockServer*)server
+{
+    NSURL* result = [NSURL URLWithString:[NSString stringWithFormat:@"ftp://127.0.0.1:%ld/%@", server.port, path]];
+    return result;
+}
+
+- (void)testContentsOfDirectoryAtURL
 {
     MockServer* server = [self setupServerWithResponses:[self ftpResponses]];
     if (server)
     {
-        NSString* listing = @"total 1\r\n-rw-------   1 user  staff     3 Mar  6  2012 file1.txt\r\n-rw-------   1 user  staff     3 Mar  6  2012 file2.txt\r\n\r\n";
-        server.data = [listing dataUsingEncoding:NSUTF8StringEncoding];
+        server.data = [ExampleListing dataUsingEncoding:NSUTF8StringEncoding];
 
         CK2FileTransferSession* session = [[CK2FileTransferSession alloc] init];
         session.delegate = self;
-        NSURL* url = [NSURL URLWithString:[NSString stringWithFormat:@"ftp://127.0.0.1:%ld/directory", (long) server.port]];
+        NSURL* url = [self URLForPath:@"/directory/" server:server];
         NSDirectoryEnumerationOptions options = NSDirectoryEnumerationSkipsSubdirectoryDescendants;
         [session contentsOfDirectoryAtURL:url includingPropertiesForKeys:nil options:options completionHandler:^(NSArray *contents, NSError *error) {
 
@@ -79,13 +86,51 @@
             }
             else
             {
-                STAssertTrue([contents count] == 2, @"should have two results");
-                NSString* file1 = [NSString stringWithFormat:@"ftp://127.0.0.1:%ld//directory/file1.txt", server.port];
-                STAssertTrue([[contents[0] absoluteString] isEqualToString:file1], @"got file 1");
-                NSString* file2 = [NSString stringWithFormat:@"ftp://127.0.0.1:%ld//directory/file2.txt", server.port];
-                STAssertTrue([[contents[1] absoluteString] isEqualToString:file2], @"got file 1");
+                NSUInteger count = [contents count];
+                STAssertTrue(count == 2, @"should have two results");
+                if (count == 2)
+                {
+                    NSURL* file1 = [self URLForPath:@"/directory/file1.txt" server:server];
+                    STAssertTrue([contents[0] isEqual:file1], @"got file 1");
+                    NSURL* file2 = [self URLForPath:@"/directory/file2.txt" server:server];
+                    STAssertTrue([contents[1] isEqual:file2], @"got file 2");
+                }
             }
             
+            [server stop];
+        }];
+
+        [server runUntilStopped];
+        [session release];
+    }
+}
+
+- (void)testEnumerateContentsOfURL
+{
+    MockServer* server = [self setupServerWithResponses:[self ftpResponses]];
+    if (server)
+    {
+        server.data = [ExampleListing dataUsingEncoding:NSUTF8StringEncoding];
+
+        CK2FileTransferSession* session = [[CK2FileTransferSession alloc] init];
+        session.delegate = self;
+        NSURL* url = [self URLForPath:@"/directory/" server:server];
+        NSDirectoryEnumerationOptions options = NSDirectoryEnumerationSkipsSubdirectoryDescendants;
+        NSMutableArray* expectedURLS = [NSMutableArray arrayWithArray:@[
+            url,
+            [self URLForPath:@"/directory/file1.txt" server:server],
+            [self URLForPath:@"/directory/file2.txt" server:server]
+        ]];
+
+        [session enumerateContentsOfURL:url includingPropertiesForKeys:nil options:options usingBlock:^(NSURL *item) {
+            NSLog(@"got item %@", item);
+            STAssertTrue([expectedURLS containsObject:item], @"got expected item");
+            [expectedURLS removeObject:item];
+        } completionHandler:^(NSError *error) {
+            if (error)
+            {
+                STFail(@"got error %@", error);
+            }
             [server stop];
         }];
 
