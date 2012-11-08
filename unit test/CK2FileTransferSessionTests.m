36cb008f9e1d53973c9e8f301a9ca3e67af5cacc
Sam Deane <sam@elegantchaos.com>
2012-11-08 11:56:01 +0000

    got directory listing test working

--- unit test/CK2FileTransferSessionTests.m
+++ unit test/CK2FileTransferSessionTests.m
@@ -26,7 +26,7 @@
     @[@"PASV", @"227 Entering Passive Mode ($pasv)\r\n"],
     @[@"SIZE test.txt", @"213 $size\r\n"],
     @[@"RETR /test.txt", @"150 Opening BINARY mode data connection for '/test.txt' ($size bytes).\r\n"],
-    @[@"LIST", @"150: Opening ASCII mode data connection for '/bin/ls'.\r\n"],
+    @[@"LIST", @(0.1), @"150 Opening ASCII mode data connection for '/bin/ls'.\r\n", @(0.1), @"226 Transfer complete.\r\n"],
     @[@"(\\w+).*", @"500 '$1': command not understood.", CloseCommand],
     ];
 
@@ -64,12 +64,28 @@
     MockServer* server = [self setupServerWithResponses:[self ftpResponses]];
     if (server)
     {
+        NSString* listing = @"total 1\r\n-rw-------   1 user  staff     3 Mar  6  2012 file1.txt\r\n-rw-------   1 user  staff     3 Mar  6  2012 file2.txt\r\n\r\n";
+        server.data = [listing dataUsingEncoding:NSUTF8StringEncoding];
+
         CK2FileTransferSession* session = [[CK2FileTransferSession alloc] init];
         session.delegate = self;
         NSURL* url = [NSURL URLWithString:[NSString stringWithFormat:@"ftp://127.0.0.1:%ld/directory", (long) server.port]];
         NSDirectoryEnumerationOptions options = NSDirectoryEnumerationSkipsSubdirectoryDescendants;
         [session contentsOfDirectoryAtURL:url includingPropertiesForKeys:nil options:options completionHandler:^(NSArray *contents, NSError *error) {
 
+            if (error)
+            {
+                STFail(@"got error %@", error);
+            }
+            else
+            {
+                STAssertTrue([contents count] == 2, @"should have two results");
+                NSString* file1 = [NSString stringWithFormat:@"ftp://127.0.0.1:%ld//directory/file1.txt", server.port];
+                STAssertTrue([[contents[0] absoluteString] isEqualToString:file1], @"got file 1");
+                NSString* file2 = [NSString stringWithFormat:@"ftp://127.0.0.1:%ld//directory/file2.txt", server.port];
+                STAssertTrue([[contents[1] absoluteString] isEqualToString:file2], @"got file 1");
+            }
+            
             [server stop];
         }];
 
