b57d12a70a9efed4a8f071c3ca28ab1415838b5b
Mike Abdullah <me@mikeabdullah.net>
2013-02-01 17:20:23 +0000

    Tiny tester app to tinker with HTTPS auth challenges Will likely remove again later

---
+++ AuthTester/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  AuthTester
+//
+//  Created by Mike on 01/02/2013.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
