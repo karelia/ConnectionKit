80701e377324ce9b364cf38173c3b7649beb44ca
Mike Abdullah <me@mikeabdullah.net>
2013-03-09 17:05:26 +0000

    Aha, need to actually test against CK2FTPProtocol

--- UnitTests/CK2CURLProtocolURLManipulationTests.m
+++ UnitTests/CK2CURLProtocolURLManipulationTests.m
@@ -148,69 +148,69 @@
 
 - (void)testFileURL;
 {
-    NSURL *url = [CK2CURLBasedProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com//test"]]
+    NSURL *url = [CK2FTPProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com//test"]]
                                                  isDirectory:NO].URL;
     STAssertEqualObjects(url.absoluteString, @"ftp://example.com//test", nil);
     
-    url = [CK2CURLBasedProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com/%2Ftest"]]
+    url = [CK2FTPProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com/%2Ftest"]]
                                           isDirectory:NO].URL;
     STAssertEqualObjects(url.absoluteString, @"ftp://example.com/%2Ftest", nil);
     
-    url = [CK2CURLBasedProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com/%2F/test"]]
+    url = [CK2FTPProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com/%2F/test"]]
                                           isDirectory:NO].URL;
     STAssertEqualObjects(url.absoluteString, @"ftp://example.com/%2F/test", nil);
 }
 
 - (void)testMakingFileIntoFolder;
 {
-    NSURL *url = [CK2CURLBasedProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com//test"]]
+    NSURL *url = [CK2FTPProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com//test"]]
                                                  isDirectory:YES].URL;
     STAssertEqualObjects(url.absoluteString, @"ftp://example.com//test/", nil);
     
-    url = [CK2CURLBasedProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com/%2Ftest"]]
+    url = [CK2FTPProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com/%2Ftest"]]
                                           isDirectory:YES].URL;
     STAssertEqualObjects(url.absoluteString, @"ftp://example.com/%2Ftest/", nil);
     
-    url = [CK2CURLBasedProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com/%2F/test"]]
+    url = [CK2FTPProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com/%2F/test"]]
                                           isDirectory:YES].URL;
     STAssertEqualObjects(url.absoluteString, @"ftp://example.com/%2F/test/", nil);
     
     // This has been giving some problems where Cocoa normally recognises the %2F as a folder, I think
-    url = [CK2CURLBasedProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com/%2F"]]
+    url = [CK2FTPProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com/%2F"]]
                                           isDirectory:YES].URL;
     STAssertEqualObjects(url.absoluteString, @"ftp://example.com/%2F/", nil);
 }
 
 - (void)testFolderURL;
 {
-    NSURL *url = [CK2CURLBasedProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com//test/"]]
+    NSURL *url = [CK2FTPProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com//test/"]]
                                                  isDirectory:YES].URL;
     STAssertEqualObjects(url.absoluteString, @"ftp://example.com//test/", nil);
     
-    url = [CK2CURLBasedProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com/%2Ftest/"]]
+    url = [CK2FTPProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com/%2Ftest/"]]
                                           isDirectory:YES].URL;
     STAssertEqualObjects(url.absoluteString, @"ftp://example.com/%2Ftest/", nil);
     
-    url = [CK2CURLBasedProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com/%2F/test/"]]
+    url = [CK2FTPProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com/%2F/test/"]]
                                           isDirectory:YES].URL;
     STAssertEqualObjects(url.absoluteString, @"ftp://example.com/%2F/test/", nil);
     
-    url = [CK2CURLBasedProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com/%2F/"]]
+    url = [CK2FTPProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com/%2F/"]]
                                           isDirectory:YES].URL;
     STAssertEqualObjects(url.absoluteString, @"ftp://example.com/%2F/", nil);
 }
 
 - (void)testMakingFolderIntoFile;
 {
-    NSURL *url = [CK2CURLBasedProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com//test/"]]
+    NSURL *url = [CK2FTPProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com//test/"]]
                                                  isDirectory:NO].URL;
     STAssertEqualObjects(url.absoluteString, @"ftp://example.com//test", nil);
     
-    url = [CK2CURLBasedProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com/%2Ftest/"]]
+    url = [CK2FTPProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com/%2Ftest/"]]
                                           isDirectory:NO].URL;
     STAssertEqualObjects(url.absoluteString, @"ftp://example.com/%2Ftest", nil);
     
-    url = [CK2CURLBasedProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com/%2F/test/"]]
+    url = [CK2FTPProtocol newRequestWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ftp://example.com/%2F/test/"]]
                                           isDirectory:NO].URL;
     STAssertEqualObjects(url.absoluteString, @"ftp://example.com/%2F/test", nil);
 }
