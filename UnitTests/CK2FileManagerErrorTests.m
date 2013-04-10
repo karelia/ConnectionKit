d4d2e551d9fc980cd005dbbfcb2fef7ff1cc7438
Sam Deane <sam@elegantchaos.com>
2013-04-10 17:19:55 +0100

    Add unit test for trying to use an unsupported scheme.

---
+++ UnitTests/CK2FileManagerErrorTests.m
@@ -0,0 +1,57 @@
+//
+//  CK2FileManagerErrorTests.m
+//  Connection
+//
+//  Created by Sam Deane on 10/04/2013.
+//  Copyright (c) 2012 Karelia Software. All rights reserved.
+//
+
+#import "CK2FileManager.h"
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CK2FileManagerErrorTests : SenTestCase
+
+@end
+
+@implementation CK2FileManagerErrorTests
+
+#pragma mark FTP
+
+- (void)testUnsupportedScheme
+{
+    CK2FileManager* fm = [[CK2FileManager alloc] init];
+
+    NSURL* url = [NSURL URLWithString:@"bogus://example.com"];
+
+    [fm contentsOfDirectoryAtURL:url includingPropertiesForKeys:@[] options:NSDirectoryEnumerationSkipsHiddenFiles completionHandler:^(NSArray *contents, NSError *error) {
+        STAssertNotNil(error, @"expecting an error");
+    }];
+
+    [fm removeItemAtURL:url completionHandler:^(NSError *error) {
+        STAssertNotNil(error, @"expecting an error");
+    }];
+
+    [fm enumerateContentsOfURL:url includingPropertiesForKeys:@[] options:NSDirectoryEnumerationSkipsHiddenFiles usingBlock:^(NSURL *url) {
+    } completionHandler:^(NSError *error) {
+        STAssertNotNil(error, @"expecting an error");
+    }];
+
+    [fm createDirectoryAtURL:url withIntermediateDirectories:YES openingAttributes:@{} completionHandler:^(NSError *error) {
+        STAssertNotNil(error, @"expecting an error");
+    }];
+
+    [fm createFileAtURL:url contents:[NSData data] withIntermediateDirectories:YES openingAttributes:@{} progressBlock:^(NSUInteger bytesWritten, NSUInteger previousAttemptCount) {
+    } completionHandler:^(NSError *error) {
+        STAssertNotNil(error, @"expecting an error");
+    }];
+
+    [fm createFileAtURL:url withContentsOfURL:url withIntermediateDirectories:YES openingAttributes:@{} progressBlock:^(NSUInteger bytesWritten, NSUInteger previousAttemptCount) {
+    } completionHandler:^(NSError *error) {
+        STAssertNotNil(error, @"expecting an error");
+    }];
+    
+    [fm release];
+}
+
+@end
