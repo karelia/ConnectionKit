a23e3cc9120bf91182f0bb50ef0f5b1be9de084e
Mike Abdullah <me@mikeabdullah.net>
2015-03-24 19:13:58 +0000

    Release blocks a little bit later
    Otherwise the cleanup can happen while we’re still delivering directory
    enumeration messages

--- ConnectionKit/CK2FileOperation.m
+++ ConnectionKit/CK2FileOperation.m
@@ -301,17 +301,18 @@ createProtocolBlock:(CK2Protocol *(^)(Class protocolClass))createBlock;
             
             // Store the error and notify completion handler
             // Make all notifications — including KVO — happen on the delegate queue
-            void (^block)(NSError*) = _completionBlock;
             
             [self tryToMessageDelegateSelector:NULL usingBlock:^(id<CK2FileManagerDelegate> delegate) { // NULL selector so always executes
                 self.error = error;
                 self.state = CK2FileOperationStateCompleted;
-                block(error);
+                _completionBlock(error);
+                
+                // Clean up now we're done notifying. Have to do this at completion time since it
+                // most likely breaks a retain cycle.
+                [_completionBlock release]; _completionBlock = nil;
+                [_progressBlock release];   _progressBlock = nil;
+                [_enumerationBlock release];_enumerationBlock = nil;
             }];
-            
-            [_completionBlock release]; _completionBlock = nil;
-            [_progressBlock release];   _progressBlock = nil;
-            [_enumerationBlock release];_enumerationBlock = nil;
             
             
             // Break retain cycle, but deliberately keep weak reference so we know we're associated with it
